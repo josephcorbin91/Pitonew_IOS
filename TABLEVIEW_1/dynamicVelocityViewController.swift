ac70785065bf25962f158b3eb0122dc258af8616
josephcorbin91 <joseph.corbin91@yahoo.com>
2017-04-14 09:27:09 -0400

    add tableview controller

--- TABLEVIEW_1/dynamicVelocityViewController.swift
+++ TABLEVIEW_1/dynamicVelocityViewController.swift
@@ -9,7 +9,7 @@
     import UIKit
     
     class dynamicVelocityViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
-        var DataSource = [String]()
+        var items = [String]()
 
          override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,17 +19,35 @@
         override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+        
+  @IBAction func insert(_ sender: UIButton) {
+        
+         items.append("Item \(items.count + 1)")
+        
+        let insertionIndexPath = NSIndexPath(forRow: items.count - 1, inSection: 0)
+        
+        tableView.insertRowsAtIndexPaths([insertionIndexPath], withRowAnimation: .Automatic)
+        
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         print("NUMBER OF ROWS  " + String(DataSource.count))
-        return DataSource.count
+        return items.count
     }
+        
+        func deleteCell(cell: UITableViewCell) {
+        if let deletionIndexPath = tableView.indexPathForCell(cell) {
+            items.removeAtIndex(deletionIndexPath.row)
+            tableView.deleteRowsAtIndexPaths([deletionIndexPath], withRowAnimation: .Automatic)
+        }
+    }
+
          
           
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             let cell : UITableViewCell
             cell = self.tableView.dequeueReusableCell(withIdentifier: "dynamicVelocityCell", for: indexPath)
-            cell.textLabel?.text = InputTitles[indexPath.row]
+            cell.textLabel?.text = items[indexPath.row]
                        return cell
        
      
