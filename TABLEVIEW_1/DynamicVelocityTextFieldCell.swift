ce56cb52622ae651f12a61c12733c8b4e4c96642
user125303 <user125303@ny093.local>
2017-04-15 09:31:04 -0400

    Done.

---
+++ TABLEVIEW_1/DynamicVelocityTextFieldCell.swift
@@ -0,0 +1,25 @@
+//
+//  DynamicVelocityTextFieldCell.swift
+//  TABLEVIEW_1
+//
+//  Created by user125303 on 4/15/17.
+//  Copyright © 2017 JCO. All rights reserved.
+//
+
+import UIKit
+
+class DynamicVelocityTextFieldCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    @IBOutlet weak var dynamicVelocityTextField: UITextField!
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
