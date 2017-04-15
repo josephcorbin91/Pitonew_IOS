b0586b6e57c455fa304773222ae8ff3a6a78115a
josephcorbin91 <joseph.corbin91@yahoo.com>
2017-04-14 20:35:58 -0400

    Update ResultViewController.swift

--- TABLEVIEW_1/ResultViewController.swift
+++ TABLEVIEW_1/ResultViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ResultViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    
+    var ResultTitles = [String]()
+    var ResultUnitsSI = [String]()
+    var ResultUnitsUS = [String]()
+    var ResultUnits = [String]()
+    var resultArray = Array(repeating: "", count: 10)
+    var SIResultsArray = Array(repeating: "", count: 10)
+    var USReaultsArray = Array(repeating: "", count: 10)
+    
     var SIResultsArray = Array(repeating: "", count: 10)
     var USReaultsArray = Array(repeating: "", count: 10)
     var currentUnits : String = ""
-    
-  
+     
     @IBAction func segmentedControlUnits(_ sender: UISegmentedControl) {
+           if(segmentedControlResults.selectedSegmentIndex == 0){
+            ResultUnits = ResultUnitsUS
+            resultArray = USReaultsArray
+
+            
+        }
+        else{
+            ResultUnits = ResultUnitsSI
+            resultArray = SIResultsArray
+            
+        }
+        tableView.reloadData()
+
     }
     @IBOutlet weak var segmentedControlResults: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
+        ResultTitles = ["Dynamic Velocity", "Average Velocity", "Mass Air Flow", "Actual Air Flow","Normal Air Flow", "Molar Weight", "Duct (P)","Area", "Atmospheric (P)", "GasDensity"]
+        ResultUnitsSI = ["m/s","m/s","kg/","m^3/s", "Nm^3/h","g/mol", "kPa", "m^2", "kPa", "kg/m^3"]
+        ResultUnitsUS = ["ft/s","ft/s","lb/min","SCFM", "ACFM","g/mol", "in Hg", "in^2", "in. Hg", "ft^3",""]
+  
         currentUnits =
         print("RESULT ARRAY")
         print(SIResultsArray)
